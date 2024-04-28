bfb3ff9a31054558d9af44b94a9435062202a4ed
Ninja2803 <89339715+Ninja2803@users.noreply.github.com>
2024-04-28 16:55:41 +0530

    monish

--- MainInterface/pdf.py
+++ MainInterface/pdf.py
@@ -1,7 +1,9 @@
-import sys
+import sys,os
+from add_bookmark import MyWindow
 from PySide6.QtWidgets import (
     QApplication,
     QMainWindow,
+    QMessageBox,
     QVBoxLayout,
     QHBoxLayout,
     QWidget,
@@ -11,14 +13,15 @@ from PySide6.QtWidgets import (
     QSizePolicy,
     QTextEdit,
     QLineEdit,
-    QSlider
+    QSlider,#
+    QDialog,QTableWidget,QTableWidgetItem,
+    QAbstractItemView  #
 )
 from PySide6.QtCore import Qt, QTimer # Added QTextCursor import
 import fitz
 import extra_functions
-from PySide6 import QtGui
-from PySide6.QtGui import QTextBlockFormat, QFont, QTextCursor 
-
+from PySide6 import QtGui,QtWidgets
+from PySide6.QtGui import QTextBlockFormat, QFont, QTextCursor ,QTextCharFormat,QColor
 class CustomButton(QPushButton):
     def __init__(self, label):
         super().__init__()
@@ -27,7 +30,7 @@ class CustomButton(QPushButton):
         font.setBold(True)
         self.setFont(font)
         self.setText(label)
-        self.setFixedSize(120, 40)  # Adjust the size as needed
+        self.setFixedSize(160, 40)# Adjust the size as needed
         self.setStyleSheet("""
             QPushButton {
                 background-color:  #353535;
@@ -42,6 +45,33 @@ class CustomButton(QPushButton):
                 background-color: #4c4c4c;
             }
         """)
+
+class CustomButton_2(QPushButton):
+    def __init__(self, label):
+        super().__init__()
+        font = QFont()
+        font.setFamilies([u"JetBrains Mono NL"])
+        font.setBold(True)
+        self.setFont(font)
+        self.setText(label)
+        self.setFixedSize(160, 80)  # Adjust the size as needed
+        self.setStyleSheet("""
+            QPushButton {
+                background-color:  #353535;
+                color: white;
+                border: none;
+                border-radius: 5px;
+                text-align: center; /* Center align text */
+            }
+            QPushButton:hover {
+                background-color: #282828;
+            }
+            QPushButton:pressed {
+                background-color: #4c4c4c;
+            }
+        """)
+
+
 
 class PDFView(QWidget):
     def __init__(self, path):
@@ -67,15 +97,14 @@ class PDFView(QWidget):
         # Set cursor position to the beginning of the text
         self.text_edit.moveCursor(QtGui.QTextCursor.Start)
 
-    def setup_ui(self):
+    def setup_ui(self): #UI Layout Function
         self.text_edit.setReadOnly(True)
         layout = QVBoxLayout(self)
         layout.addWidget(self.text_edit)
 
     def get_selected_text(self):
         return self.text_edit.textCursor().selectedText()
-
-
+    
 class InfoLabel(QWidget):
     def __init__(self):
         super().__init__()
@@ -93,13 +122,24 @@ class ButtonHolder(QWidget):
     def __init__(self, main_window):
         super().__init__()
         self.main_window = main_window
+        self.stored_signal=None
 
         self.meaning_button = CustomButton("Meaning")
         self.meaning_button.clicked.connect(self.on_meaning_button_clicked)
 
         self.synonym_button = CustomButton("Synonym")
         self.synonym_button.clicked.connect(self.on_synonym_button_clicked)
+        
+        # Creating the zoom in button 
+        self.zoomInButton = CustomButton("+")
+        self.zoomInButton.clicked.connect(self.zoomIn)
+        self.zoomInButton.setFixedSize(60,40) # Defining the size of the button
 
+        # Creating the zoom out button 
+        self.zoomOutButton = CustomButton("-")
+        self.zoomOutButton.clicked.connect(self.zoomOut)
+        self.zoomOutButton.setFixedSize(60,40) # Defining the size of the button
+    
         self.speech_button = CustomButton("Read Aloud")
         self.speech_button.clicked.connect(self.on_speech_button_clicked)
         
@@ -138,11 +178,40 @@ class ButtonHolder(QWidget):
         self.prev_button.clicked.connect(self.prev_occurrence)
         self.prev_button.setFixedSize(60,40)
 
+        self.add_bookmark_button = CustomButton_2("Bookmarks \n (Select a text )") 
+        self.add_bookmark_button.clicked.connect(self.add_bookmark)
+
+        self.go_to_bookmark_button= CustomButton("Go to Bookmark")
+        self.go_to_bookmark_button.clicked.connect(self.go_to_bookmark)
+
+        # Creating a highlight button 
+        self.highlight = CustomButton('Highlight')
+        self.highlight.clicked.connect(self.highlight_text)
+
+
+        # Loading the highlights done in the previous session 
+        self.load_highlighted_text()
+
+        # Creating a remove highlighting button 
+        self.remove = CustomButton('Clear')
+        self.remove.clicked.connect(self.remove_text)
+
         self.navigate_layout=QHBoxLayout()
         self.navigate_layout.addWidget(self.prev_button)
         self.navigate_layout.addWidget(self.next_button)
 
+        # Creating the layout for zooming buttons
+        self.zoom = QHBoxLayout()
+        self.zoom.addWidget(self.zoomInButton)
+        self.zoom.addWidget(self.zoomOutButton)
+
+        # Creating the border for the zooming button layout
+        self.border = QWidget()
+        self.border.setLayout(self.zoom)
+        self.border.setStyleSheet("QWidget { border: 2px solid red; }")
+
         self.search_results = []
+        self.search_result=[]
         self.current_pos = -1
         self.container = QVBoxLayout()
         self.container.addWidget(self.meaning_button)
@@ -155,6 +224,12 @@ class ButtonHolder(QWidget):
         self.container.addWidget(self.search_box)
         self.container.addWidget(self.search_button)
         self.container.addLayout(self.navigate_layout)
+        self.container.addWidget(self.add_bookmark_button)
+        self.container.addWidget(self.go_to_bookmark_button)
+        self.container.addWidget(self.border) # Adding the border for the zooming buttons
+        self.container.addLayout(self.zoom) # Adding the layout for the zooming buttons
+        self.container.addWidget(self.highlight) # Adding the highlighting button
+        self.container.addWidget(self.remove) # Adding the remove highlighting button
         self.container.addWidget(self.close_button)
         self.container.addItem(QSpacerItem(0, 0, QSizePolicy.Minimum, QSizePolicy.Expanding))
         self.setLayout(self.container)
@@ -182,12 +257,76 @@ class ButtonHolder(QWidget):
             self.main_window.start_auto_scroll()
         else:
             self.main_window.stop_auto_scroll()
+    
+    def zoomIn(self):
+        font = self.main_window.pdf_view.text_edit.font() 
+        font_size = font.pointSize() # Getting the font size
+        font.setPointSize(font_size + 1) # Increasing the font size for zooming In
+        self.main_window.pdf_view.text_edit.setFont(font) # Applying the changes
+
+    def zoomOut(self):
+        font = self.main_window.pdf_view.text_edit.font()
+        font_size = font.pointSize() # Getting the font size
+        font.setPointSize(font_size - 1) # Increasing the font size for zooming out
+        self.main_window.pdf_view.text_edit.setFont(font) # Applying the changes
+    
+    def highlight_text(self):
+        selected_text = self.main_window.pdf_view.get_selected_text() # Getting the selected text
+        if selected_text:
+            # Get the current page number based on the cursor position
+            cursor = self.main_window.pdf_view.text_edit.textCursor() # Creating a cursor to navigate the document
+            format = QTextCharFormat() # Creating a format to apply on the text
+            format.setBackground(QColor("red")) 
+            cursor.mergeCharFormat(format) # Applying the formating on the selected text
+            file_path = "highlights.txt"
+            L = [] # Appending the details in a list 
+            L.append([selected_text])
+            file_path = "highlights.txt"  # Save highlights to a text file
+            with open(file_path, "w") as file:
+                for i in L:
+                    file.write(",".join(map(str, i)) + "\n")
+        else:
+            QMessageBox.information(self,"No Text selected ","Select a portion of the document") # Prompting the user to select some text
+
+
+    def remove_text(self):
+        selected_text = self.main_window.pdf_view.get_selected_text() # Getting the selected text
+        if selected_text:
+            # Get the current page number based on the cursor position
+            cursor = self.main_window.pdf_view.text_edit.textCursor() # Creating a cursor to navigate the document
+            format = QTextCharFormat() # Creating the default format to apply on the text
+            format.setBackground(QColor("Transparent"))   
+            cursor.mergeCharFormat(format) # Applying the formating on the selected text
+        else:
+            QMessageBox.information(self,"No Text selected ","Select a portion of the document") # Prompting the user to select some text
+
+    def load_highlighted_text(self):
+        try:
+            # Reading the file in which highlights are stored and appending to a list
+            loaded_highlights = []
+            file_path = "highlights.txt"
+            with open(file_path, "r") as file:
+                for line in file:
+                    loaded_highlights.append(list(map(str.strip, line.split(","))))
+            # Reading the list and applying the formating 
+            for i in loaded_highlights:
+                selected_text = i[0]
+                cursor = self.main_window.pdf_view.text_edit.textCursor() # Creating a cursor to navigate the document
+                color = "red"
+                format = QTextCharFormat() # Creating the default format to apply on the text
+                format.setBackground(QColor(color))
+                cursor = self.main_window.pdf_view.text_edit.document().find(selected_text, cursor) # Moving the cursor the location of selected text
+                cursor.mergeCharFormat(format)
+        except FileNotFoundError:
+            pass  # No highlighted text information available
 
     def on_speed_changed(self, value):
         self.main_window.set_auto_scroll_speed(value)
 
+
     def search_pdf(self):
         search_query = self.search_box.text()
+        print(search_query,type(search_query))
         if search_query:
             # Set the cursor to the start of the document
             cursor = self.main_window.pdf_view.text_edit.textCursor()
@@ -218,9 +357,12 @@ class ButtonHolder(QWidget):
     def next_occurrence(self):
         if self.search_results:
             self.current_pos = (self.current_pos + 1) % len(self.search_results)
+            print(self.current_pos)
             self.main_window.pdf_view.text_edit.setTextCursor(self.search_results[self.current_pos])
             self.main_window.pdf_view.text_edit.ensureCursorVisible()
 
+    
+   
     def prev_occurrence(self):
         if self.search_results:
             self.current_pos = (self.current_pos - 1) % len(self.search_results)
@@ -228,13 +370,170 @@ class ButtonHolder(QWidget):
             self.main_window.pdf_view.text_edit.ensureCursorVisible()
 
 
+    def add_bookmark(self):
+
+        selected_text = self.main_window.pdf_view.get_selected_text()
+        if selected_text:
+            # Get the current page number based on the cursor position
+            cursor = self.main_window.pdf_view.text_edit.textCursor()
+            current_page = cursor.blockNumber() + 1 
+            current_dir = os.getcwd()   
+            pdf_name=os.path.splitext(os.path.basename(self.main_window.pdf_view.pdf_path))[0]
+            bookmark_window = MyWindow(current_page,pdf_name,current_dir,selected_text)  # This creates the window for adding a bookmark
+            bookmark_window.setGeometry(100, 100, 900, 400)  # Adjust geometry as needed
+            bookmark_window.show()  # Show the window
+
+            # Call exec_ to ensure the application event loop is properly executed
+            bookmark_window.exec()
+        else:
+            QMessageBox.information(self,"No Text selected "," please select a text to add bookmark")
+
+    def go_to_bookmark(self):
+        current_dir = os.getcwd()   
+        pdf_name=os.path.splitext(os.path.basename(self.main_window.pdf_view.pdf_path))[0]
+        dialog=open_bookmark(pdf_name,current_dir)
+        result = dialog.exec()
+        if result == QDialog.Accepted:
+             selected_page_number,selected_word = dialog.get_selected_bookmark()
+             if selected_page_number is not None:
+                 self.search_bookmark(selected_page_number,selected_word)
+
+    def search_bookmark(self, page_number, word):
+
+        if word:
+            # Set the cursor to the start of the document
+            cursor = self.main_window.pdf_view.text_edit.textCursor()
+            cursor.setPosition(0)
+            self.main_window.pdf_view.text_edit.setTextCursor(cursor)
+            
+            # Clear previous search results
+            self.search_result.clear()
+            self.current_pos = -1
+            while True:
+                found_cursor = self.main_window.pdf_view.text_edit.document().find(word, cursor)
+                if found_cursor.isNull():
+                    break
+                self.search_result.append(found_cursor)
+                cursor = found_cursor
+            
+            # Search for all occurrences of the word
+            if self.search_result:
+                     self.current_pos=0
+                # Move the cursor to the first occurrence
+                     new_cursor = self.main_window.pdf_view.text_edit.textCursor()
+                     current_page = new_cursor.blockNumber() + 1
+                     while (current_page!=page_number) :
+                              self.current_pos = (self.current_pos + 1) % len(self.search_result)
+                              self.main_window.pdf_view.text_edit.setTextCursor(self.search_result[self.current_pos])
+                              self.main_window.pdf_view.text_edit.ensureCursorVisible()
+                              cursor = self.main_window.pdf_view.text_edit.textCursor()
+                              current_page = cursor.blockNumber() + 1
+            else:
+                self.main_window.information.print_data("Bookmark not found.")
+
+class Data_Bookmark:
+    def __init__(self, bookmark_name, page_number, notes,word):
+        self.bookmark_name = bookmark_name
+        self.page_number = page_number
+        self.notes = notes
+        self.word=word
+
+class open_bookmark(QDialog):
+    def __init__(self, pdf_name, current_dir):
+        super().__init__()
+        self.pdf_name = pdf_name
+        self.current_dir = current_dir
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.setWindowTitle("Open Bookmark")
+
+        self.table_widget = QTableWidget(self)
+        self.table_widget.setColumnCount(4)
+        self.table_widget.setHorizontalHeaderLabels(["Bookmark Name", "Page Number", "Notes"])
+
+        self.table_widget.setColumnWidth(0, 150)  # Bookmark Name
+        self.table_widget.setColumnWidth(1, 80)  # Page Number
+        self.table_widget.setColumnWidth(2, 250)  # Notes
+        self.table_widget.setColumnWidth(3, 250)  # Hint
+
+        # Set the row height for better visibility of multi-line notes
+        self.table_widget.verticalHeader().setDefaultSectionSize(50)
+
+        # Set selection mode to select entire rows
+        self.table_widget.setSelectionBehavior(QAbstractItemView.SelectRows)
+
+        self.open_button = QPushButton("Open Bookmark", self)
+        self.open_button.clicked.connect(self.accept)
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.table_widget)
+        layout.addWidget(self.open_button)
+
+        self.load_bookmarks()
+        self.setFixedSize(900, 400)
+
+    def add_bookmark_to_table(self, bookmark):
+
+        row_position = self.table_widget.rowCount()
+        self.table_widget.insertRow(row_position)
+
+        # Insert bookmark's details into the table
+        self.table_widget.setItem(row_position, 0, QtWidgets.QTableWidgetItem(bookmark.bookmark_name))
+        self.table_widget.setItem(row_position, 1, QtWidgets.QTableWidgetItem(str(bookmark.page_number)))
+        self.table_widget.setItem(row_position, 2, QtWidgets.QTableWidgetItem(bookmark.notes))
+        self.table_widget.setItem(row_position, 3, QtWidgets.QTableWidgetItem(bookmark.word))
+
+    def load_bookmarks(self):
+        # Define the file name based on the PDF name
+        filename = f"{self.pdf_name}_bookmarks.txt"
+        # Get the current directory
+        filepath = os.path.join(self.current_dir, filename)
+        # Open the file in read mode
+        try:
+            with open(filepath, "r+",encoding="utf-8") as file:
+                # Read each line from the file
+                for line in file:
+                    # Split the line into bookmark details
+                    bookmark_name, page_number, notes,word = line.strip().split(';')
+                    page_number = int(page_number)
+                    # Replace placeholder with newline character
+                    notes = notes.replace('<br>', '\n')
+                    word = word.replace('<br>', '\n')
+                    # Create bookmark object
+                    bookmark = Data_Bookmark(bookmark_name, page_number, notes, word)
+                    # Add bookmark to the table
+                    self.add_bookmark_to_table(bookmark)
+
+        except FileNotFoundError:
+            # If the file doesn't exist, create it
+            with open(filepath, "w", encoding="utf-8"):
+                pass  # Do nothing, file created
+
+
+    def get_selected_bookmark(self):
+        selected_items = self.table_widget.selectedItems()
+        if selected_items:
+            row = selected_items[0].row()
+            page_number = int(self.table_widget.item(row, 1).text())
+            word = self.table_widget.item(row, 3).text().split()
+            return [page_number,word[0]]
+        return None
+
+
+    
+
 class MainWindow(QWidget):
+
+
     def __init__(self, path):
         super().__init__()
         self.setWindowTitle("PySide6 PDF Viewer")
         self.pdf_view = PDFView(path)
         self.information = InfoLabel()
         self.button_holder = ButtonHolder(self)
+
+            
 
         self.qlayout = QVBoxLayout()
         self.qlayout.addWidget(self.pdf_view.text_edit, 10)
@@ -310,6 +609,12 @@ class MainWindow(QWidget):
             new_scroll = 0
         # Set new scroll position
         self.pdf_view.text_edit.verticalScrollBar().setValue(new_scroll)
+
+
+
+    
+
+
 
 
 if __name__ == "__main__":
